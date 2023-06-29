b88931d50c0248bc35928a7560401d2354849fdc
GIVEN MNISI <givenmnisi6@gmail.com>
2023-06-29 14:29:26 +0200

    Merging branches

--- Cobit-19/Models/AppDbContext.cs
+++ Cobit-19/Models/AppDbContext.cs
@@ -309,20 +309,14 @@ namespace Cobit_19.Models
             {
                 for (int j = 0; j < 4; j++)
                 {
-
                     {
-
                         float dsweight1 = weights1[i, j];
-
                         builder.Entity<MapModel>().HasData(
                         new MapModel { QuestionID = j + 1, ObjectiveID = i + 1, Weight = dsweight1 }
                         );
                     };
-
-
                 }
             }
-
 
 
             /// Mapping 19 Questions and 40 Objectives with their weights for design factor 3
@@ -377,21 +371,201 @@ namespace Cobit_19.Models
             {
                 for (int j = 0; j < 19; j++)
                 {
-
                     {
-
                         float dsweight3 = weights3[i, j];
-
                         builder.Entity<MapModel>().HasData(
                         new MapModel { ObjectiveID = i + 1, QuestionID = j + 18, Weight = dsweight3 }
                         );
                     };
 
-
                 }
             }
 
 
+
+            float[,] weights4 = new float[,]
+            {
+                { 3.0f, 3.0f, 1.0f, 1.0f, 2.0f, 2.0f, 2.0f, 1.0f, 1.0f, 1.0f, 3.0f, 3.5f, 1.0f, 1.0f, 1.0f, 1.0f, 2.0f, 3.0f, 1.5f, 1.0f },    // EDM01
+                { 2.5f, 3.0f, 1.0f, 1.0f, 1.5f, 2.5f, 2.0f, 1.5f, 0.5f, 2.5f, 1.5f, 1.0f, 3.0f, 2.0f, 1.0f, 1.0f, 2.0f, 2.0f, 1.0f, 2.5f },    // EDM02
+                { 1.0f, 1.0f, 2.0f, 1.0f, 2.0f, 2.0f, 1.0f, 1.0f, 0.0f, 0.5f, 1.0f, 0.0f, 1.0f, 1.5f, 1.0f, 2.5f, 1.0f, 1.0f, 2.5f, 1.0f },    // EDM03
+                { 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 2.0f, 3.0f, 3.5f, 3.5f, 1.0f, 1.5f, 0.0f, 4.0f, 2.0f, 1.0f, 1.5f, 2.0f, 2.5f, 0.0f, 1.0f },    // EDM04
+                { 1.0f, 1.0f, 1.0f, 1.0f, 1.5f, 2.0f, 1.0f, 1.0f, 0.0f, 1.0f, 3.0f, 1.5f, 1.5f, 0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 1.0f, 0.0f },    // EDM05
+
+
+                { 2.0f, 1.0f, 2.0f, 1.0f, 2.0f, 2.0f, 1.0f, 1.0f, 0.0f, 0.5f, 1.5f, 4.0f, 1.0f, 2.0f, 1.0f, 1.0f, 1.5f, 2.0f, 0.5f, 1.0f },    // APO01
+                { 1.5f, 1.5f, 1.5f, 1.5f, 1.0f, 1.5f, 1.0f, 1.0f, 0.0f, 1.0f, 2.5f, 0.5f, 0.5f, 1.5f, 1.5f, 0.5f, 2.0f, 2.0f, 0.0f, 2.5f },    // APO02
+                { 1.0f, 1.5f, 1.0f, 2.0f, 0.5f, 1.5f, 2.0f, 1.5f, 1.0f, 3.5f, 0.5f, 0.5f, 1.0f, 4.0f, 1.0f, 3.5f, 2.0f, 3.0f, 0.0f, 2.0f },    // APO03
+                { 1.0f, 1.0f, 1.0f, 1.0f, 0.5f, 0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.5f, 1.0f, 0.5f, 2.0f, 1.0f, 0.0f, 0.5f, 0.5f, 0.0f, 4.0f },    // APO04
+                { 3.0f, 3.0f, 1.0f, 1.5f, 2.0f, 2.0f, 1.5f, 3.5f, 0.5f, 2.0f, 2.0f, 1.5f, 2.0f, 1.0f, 0.5f, 0.0f, 2.5f, 2.5f, 0.0f, 2.0f },    // APO05
+
+                { 3.5f, 2.0f, 1.0f, 1.5f, 1.5f, 2.0f, 4.0f, 3.0f, 1.0f, 2.0f, 1.0f, 1.5f, 4.0f, 0.0f, 0.0f, 0.0f, 1.0f, 2.0f, 0.0f, 0.0f },    // APO06
+                { 1.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.5f, 2.0f, 2.0f, 4.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 3.0f, 0.0f, 0.5f, 0.5f, 1.5f, 1.0f },    // APO07
+                { 2.5f, 2.0f, 1.0f, 2.5f, 1.5f, 1.0f, 2.5f, 2.0f, 1.5f, 1.0f, 3.0f, 1.0f, 0.5f, 1.0f, 4.0f, 1.0f, 3.0f, 3.5f, 0.0f, 0.5f },    // APO08
+                { 2.0f, 1.5f, 2.0f, 4.0f, 1.0f, 2.5f, 1.5f, 2.0f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.5f, 0.0f, 0.0f },    // APO09
+                { 1.0f, 1.0f, 2.0f, 4.0f, 1.5f, 1.5f, 1.5f, 1.0f, 1.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.5f, 2.0f, 1.0f, 0.0f },    // APO10
+
+                { 1.0f, 1.0f, 3.0f, 1.5f, 1.0f, 3.0f, 0.0f, 0.0f, 0.0f, 2.0f, 0.0f, 0.0f, 0.0f, 0.5f, 0.5f, 3.0f, 2.0f, 2.0f, 0.0f, 1.0f },    // APO11
+                { 1.0f, 0.5f, 2.5f, 1.5f, 2.0f, 2.0f, 1.0f, 1.0f, 0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 2.0f, 1.0f, 1.5f, 2.5f, 1.0f },    // APO12
+                { 0.0f, 0.0f, 3.5f, 1.0f, 2.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.5f, 2.0f, 1.0f, 2.0f, 1.0f },    // APO13
+                { 1.0f, 1.0f, 3.0f, 1.0f, 2.5f, 1.5f, 1.0f, 1.5f, 0.0f, 1.5f, 0.0f, 0.0f, 0.5f, 2.5f, 0.5f, 4.0f, 2.5f, 2.0f, 3.0f, 0.0f },    // APO14
+
+                { 0.0f, 1.0f, 1.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 3.0f, 1.0f, 3.5f, 0.0f, 0.0f, 1.5f, 0.0f, 0.0f, 1.5f, 2.0f, 0.0f, 1.0f },    // BAI01
+                { 0.0f, 3.0f, 0.0f, 0.0f, 0.5f, 0.5f, 2.0f, 0.0f, 2.0f, 0.0f, 3.5f, 0.0f, 1.0f, 1.0f, 2.0f, 1.5f, 2.5f, 3.0f, 0.5f, 1.0f },    // BAI02
+                { 1.0f, 2.0f, 2.0f, 0.0f, 0.0f, 0.0f, 2.0f, 0.0f, 1.0f, 0.0f, 3.0f, 0.0f, 0.5f, 1.0f, 1.0f, 0.5f, 2.0f, 2.0f, 1.0f, 0.5f },    // BAI03
+                { 0.5f, 0.0f, 2.0f, 3.0f, 0.0f, 0.0f, 2.0f, 0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.5f },    // BAI04
+
+                { 1.0f, 3.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.5f, 0.0f, 3.0f, 1.0f, 0.0f, 0.0f, 0.5f, 2.0f, 0.0f, 0.5f, 1.5f, 0.0f, 1.0f },    // BAI05
+                { 0.0f, 0.0f, 2.5f, 3.0f, 0.5f, 1.5f, 0.0f, 1.0f, 0.0f, 1.5f, 0.0f, 1.0f, 0.5f, 1.0f, 0.5f, 2.0f, 2.0f, 2.0f, 1.0f, 1.0f },    // BAI06
+                { 0.0f, 1.0f, 2.0f, 2.0f, 0.5f, 1.5f, 0.0f, 0.5f, 0.0f, 2.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.5f, 2.0f, 2.0f, 2.0f, 0.0f, 1.0f },    // BAI07
+                { 0.0f, 0.0f, 0.0f, 1.5f, 0.5f, 0.5f, 0.0f, 1.0f, 2.0f, 0.5f, 0.0f, 0.5f, 0.0f, 1.0f, 3.0f, 2.0f, 1.0f, 1.5f, 0.0f, 0.5f },    // BAI08
+
+
+                { 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 2.0f, 2.0f, 0.0f, 0.0f, 0.0f, 0.0f, 2.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.5f, 0.0f, 0.0f },    // BAI09
+                { 0.0f, 0.0f, 2.5f, 2.0f, 0.5f, 0.0f, 0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.5f, 0.0f, 1.5f, 1.0f, 2.0f, 0.0f, 0.0f },    // BAI10
+                { 1.0f, 2.0f, 2.5f, 0.0f, 0.0f, 0.0f, 2.0f, 3.0f, 1.0f, 4.0f, 0.0f, 0.0f, 1.5f, 2.0f, 0.5f, 0.0f, 1.0f, 1.5f, 0.0f, 0.5f },    // BAI11
+
+                { 0.0f, 0.0f, 2.5f, 2.0f, 1.0f, 2.0f, 0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.5f, 1.0f, 2.0f, 0.0f, 0.0f },    // DSS01
+                { 1.0f, 1.0f, 4.0f, 3.0f, 1.0f, 2.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f },    // DSS02
+                { 0.0f, 1.0f, 3.0f, 3.0f, 0.0f, 3.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.5f, 1.0f, 1.0f, 1.0f, 0.5f, 0.0f },    // DSS03
+
+                { 0.0f, 0.0f, 3.0f, 1.0f, 2.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.5f, 1.0f, 2.0f, 0.0f, 0.0f },    // DSS04
+                { 0.0f, 0.0f, 4.0f, 2.0f, 2.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.5f, 1.0f, 2.0f, 2.0f, 0.0f },    // DSS05
+                { 0.0f, 1.0f, 0.5f, 0.0f, 3.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.5f, 2.5f, 1.5f, 1.0f, 2.0f, 0.0f },    // DSS06
+
+                { 1.0f, 1.5f, 2.0f, 2.0f, 2.5f, 3.0f, 1.0f, 2.0f, 1.5f, 1.0f, 1.0f, 1.0f, 2.0f, 1.0f, 1.0f, 1.0f, 1.5f, 1.0f, 2.5f, 1.0f },    // MEA01
+                { 0.0f, 0.0f, 2.0f, 2.0f, 2.5f, 2.0f, 2.0f, 0.0f, 0.5f, 2.0f, 1.0f, 1.0f, 1.5f, 1.0f, 0.0f, 2.0f, 1.0f, 1.0f, 2.5f, 0.0f },    // MEA02
+                { 0.0f, 0.0f, 2.0f, 2.0f, 4.0f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 2.0f, 0.0f, 0.0f, 4.0f, 0.0f },    // MEA03
+                { 1.0f, 1.0f, 3.0f, 1.5f, 3.0f, 4.0f, 2.0f, 1.0f, 1.0f, 0.5f, 1.0f, 1.0f, 1.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 2.5f, 1.0f }     // MEA04
+            };
+
+            for (int i = 0; i < 40; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    {
+                        float dsweight4 = weights4 [i, j];
+                        builder.Entity<MapModel>().HasData(
+                        new MapModel { ObjectiveID = i + 1, QuestionID = j + 1, Weight = dsweight4 }
+                        );
+                    };
+                }
+            }
+
+            float[,] weights5 = new float[,]
+            {
+                { 3.0f, 1.0f },    // EDM01
+                { 1.0f, 1.0f },    // EDM02
+                { 4.0f, 1.0f },    // EDM03
+                { 1.5f, 1.0f },    // EDM04
+                { 2.0f, 1.0f },    // EDM05
+                { 3.0f, 1.0f },    // APO01
+                { 1.0f, 1.0f },    // APO02
+                { 3.0f, 1.0f },    // APO03
+                { 1.0f, 1.0f },    // APO04
+                { 1.0f, 1.0f },    // APO05
+                { 1.0f, 1.0f },    // APO06
+                { 2.0f, 1.0f },    // APO07
+                { 1.0f, 1.0f },    // APO08
+                { 2.0f, 1.0f },    // APO09
+                { 3.0f, 1.0f },    // APO10
+                { 2.0f, 1.0f },    // APO11
+                { 4.0f, 1.0f },    // APO12
+                { 4.0f, 1.0f },    // APO13
+                { 3.0f, 1.0f },    // APO14
+                { 1.0f, 1.0f },    // BAI01
+                { 1.0f, 1.0f },    // BAI02
+                { 1.0f, 1.0f },    // BAI03
+                { 2.0f, 1.0f },    // BAI04
+                { 1.0f, 1.0f },    // BAI05
+                { 3.0f, 1.0f },    // BAI06
+                { 1.0f, 1.0f },    // BAI07
+                { 1.0f, 1.5f },    // BAI08
+                { 1.0f, 1.0f },    // BAI09
+                { 3.0f, 1.0f },    // BAI10
+                { 1.0f, 1.0f },    // BAI11
+                { 1.0f, 1.0f },    // DSS01
+                { 3.0f, 1.0f },    // DSS02
+                { 2.0f, 1.0f },    // DSS03
+                { 4.0f, 1.0f },    // DSS04
+                { 3.0f, 1.0f },    // DSS05
+                { 3.0f, 1.0f },    // DSS06
+                { 3.0f, 1.0f },    // MEA01
+                { 2.0f, 1.0f },    // MEA02
+                { 3.0f, 1.0f },    // MEA03
+                { 3.0f, 1.0f }     // MEA04
+            };
+
+            for (int i = 0; i < 40; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    {
+                        float dsweight5 = weights5 [i, j];
+                        builder.Entity<MapModel>().HasData(
+                        new MapModel { ObjectiveID = i + 1, QuestionID = j + 1, Weight = dsweight5 }
+                        );
+                    };
+                }
+            }
+
+            float[,] weights6 = new float[,]
+            {
+                { 3.0f, 2.0f, 1.0f },    // EDM01
+                { 1.0f, 1.0f, 1.0f },    // EDM02
+                { 4.0f, 2.0f, 1.0f },    // EDM03
+                { 1.0f, 1.0f, 1.0f },    // EDM04
+                { 1.5f, 1.0f, 1.0f },    // EDM05
+                { 2.0f, 1.5f, 1.0f },    // APO01
+                { 1.0f, 1.0f, 1.0f },    // APO02
+                { 1.0f, 1.0f, 1.0f },    // APO03
+                { 1.0f, 1.0f, 1.0f },    // APO04
+                { 1.0f, 1.0f, 1.0f },    // APO05
+                { 1.0f, 1.0f, 1.0f },    // APO06
+                { 1.0f, 1.0f, 1.0f },    // APO07
+                { 1.0f, 1.0f, 1.0f },    // APO08
+                { 1.0f, 1.0f, 1.0f },    // APO09
+                { 1.5f, 1.0f, 1.0f },    // APO10
+                { 1.0f, 1.0f, 1.0f },    // APO11
+                { 4.0f, 2.0f, 1.0f },    // APO12
+                { 1.5f, 1.0f, 1.0f },    // APO13
+                { 2.0f, 1.5f, 1.0f },    // APO14
+                { 1.0f, 1.0f, 1.0f },    // BAI01
+                { 1.0f, 1.0f, 1.0f },    // BAI02
+                { 1.0f, 1.0f, 1.0f },    // BAI03
+                { 1.0f, 1.0f, 1.0f },    // BAI04
+                { 1.0f, 1.0f, 1.0f },    // BAI05
+                { 1.0f, 1.0f, 1.0f },    // BAI06
+                { 1.0f, 1.0f, 1.0f },    // BAI07
+                { 1.0f, 1.5f, 1.5f },    // BAI08
+                { 1.0f, 1.0f, 1.0f },    // BAI09
+                { 1.0f, 1.0f, 1.0f },    // BAI10
+                { 1.0f, 1.0f, 1.0f },    // BAI11
+                { 1.0f, 1.0f, 1.0f },    // DSS01
+                { 1.0f, 1.0f, 1.0f },    // DSS02
+                { 1.0f, 1.0f, 1.0f },    // DSS03
+                { 1.0f, 1.0f, 1.0f },    // DSS04
+                { 2.0f, 1.0f, 1.0f },    // DSS05
+                { 1.0f, 1.0f, 1.0f },    // DSS06
+                { 1.0f, 1.0f, 1.0f },    // MEA01
+                { 1.0f, 1.0f, 1.0f },    // MEA02
+                { 4.0f, 2.0f, 1.0f },    // MEA03
+                { 3.5f, 2.0f, 1.0f }     // MEA04
+            };
+
+            //nested for loop for all the 40 Objectives
+            for (int i = 0; i < 40; i++)
+            {
+                // j<3 - set of questions
+                for (int j = 0; j < 3; j++)
+                {
+                    {
+                        float dsweight6 = weights6 [i, j];
+                        builder.Entity<MapModel>().HasData(
+                        new MapModel { ObjectiveID = i + 1, QuestionID = j + 1, Weight = dsweight6 }
+                        );
+                    };
+                }
+            }
 
             /// Mapping 4 Questions and 40 Objectives with their weights for design factor 7
             float[,] weights7 = new float[,]
@@ -440,20 +614,14 @@ namespace Cobit_19.Models
 
             for (int i = 0; i < 40; i++)
             {
-
                 for (int j = 0; j < 4; j++)
                 {
-
                     {
-
                         float dsweight7 = weights7[i, j];
-
                         builder.Entity<MapModel>().HasData(
                         new MapModel { ObjectiveID = i + 1, QuestionID = j + 62, Weight = dsweight7 }
                         );
-                    };
-
-
+                    }
                 }
             }
 
@@ -512,17 +680,12 @@ namespace Cobit_19.Models
 
                 for (int j = 0; j < 3; j++)
                 {
-
                     {
-
                         float dsweight7 = weights8[i, j];
-
                         builder.Entity<MapModel>().HasData(
                         new MapModel { ObjectiveID = i + 1, QuestionID = j + 66, Weight = dsweight7 }
                         );
                     };
-
-
                 }
             }
 
@@ -577,17 +740,13 @@ namespace Cobit_19.Models
 
                 for (int j = 0; j < 3; j++)
                 {
-
                     {
-
                         float dsweight7 = weights9[i, j];
 
                         builder.Entity<MapModel>().HasData(
                         new MapModel { ObjectiveID = i + 1, QuestionID = j + 69, Weight = dsweight7 }
                         );
                     };
-
-
                 }
             }
 
@@ -641,17 +800,13 @@ namespace Cobit_19.Models
 
                 for (int j = 0; j < 3; j++)
                 {
-
                     {
-
                         float dsweight7 = weights10[i, j];
 
                         builder.Entity<MapModel>().HasData(
                         new MapModel { ObjectiveID = i + 1, QuestionID = j + 72, Weight = dsweight7 }
                         );
-                    };
-
-
+                    }
                 }
             }
 
