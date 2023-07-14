510df24a313bc345c71a6a91b71f54a4e02b1635
Daniel <danicoetzee1@gmail.com>
2023-07-14 20:07:40 +0200

    Started working on a user management provider.

---
+++ Cobit-19/Business/Admin/UserManagementProvider.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Cobit_19.Data.Models;
+using Cobit_19.Shared.Dtos;
+using Microsoft.AspNetCore.Identity;
+
+namespace Cobit_19.Business.Admin
+{
+    public class UserManagementProvider
+    {
+        private readonly IMapper _mapper;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<ApplicationUser> _roleManager;
+
+        public UserManagementProvider(IMapper mapper, UserManager<ApplicationUser> userManager, RoleManager<ApplicationUser> roleManager)
+        {
+            _mapper = mapper;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IEnumerable<UserDto> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserDto GetUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CreateUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateUser()
+        {
+            throw new NotImplementedException();
+        }
+        public bool DeleteUser()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
