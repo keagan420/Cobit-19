bb10c73d9e7a5a8dbc03e3e0e25330d89ba0b38e
Gerni Visser <gerni.visser@gmail.com>
2023-07-19 18:11:02 +0200

    Added Mapping Service

---
+++ Cobit-19/Business/Audits/MappingService.cs
@@ -0,0 +1,46 @@
+﻿using Cobit_19.Shared.Dtos;
+
+namespace Cobit_19.Business.Audits
+{
+    public static class MappingService
+    {
+        public static List<ObjectiveValueDto> Calculate(DesignFactorDto designFactorDto, List<ObjectiveDto> objectives)
+        {
+            List<ObjectiveValueDto> objectiveValues = new List<ObjectiveValueDto>();
+
+            foreach (var objective in objectives)
+            {
+                var objectiveValue = new ObjectiveValueDto()
+                {
+                    Objective = objective.Name,
+                    Score = 0,
+                    BaselineScore = 0,
+                    RelativeInportance = 0
+                };
+
+                foreach (var question in designFactorDto.Questions)
+                {
+                    float weight = question.Maps.Where(m => m.ObjectiveID == objective.ID).First().Weight;
+                    float answer = question.Answers.First().Answer;
+                    float baseline = question.BaseAnswer;
+
+                    objectiveValue.Score += weight * answer;
+                    objectiveValue.BaselineScore += weight * baseline;
+
+                    // If odds is not 0, multiply the value by the odds
+                    if(question.Answers.First().Odds != 0)
+                    {
+                        objectiveValue.Score *= question.Answers.First().Odds;
+                        objectiveValue.BaselineScore *= question.Answers.First().Odds;
+                    }
+                }
+
+                objectiveValue.RelativeInportance = objectiveValue.Score - objectiveValue.BaselineScore;
+
+                objectiveValues.Add(objectiveValue);
+            }
+
+            return objectiveValues;
+        }
+    }
+}
---
+++ Cobit-19/Shared/Dtos/ObjectiveValueDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Cobit_19.Shared.Dtos
+{
+    public class ObjectiveValueDto
+    {
+        public string Objective { get; set; }
+        public float Score { get; set; }
+        public float BaselineScore { get; set; }
+        public float RelativeInportance { get; set; } 
+    }
+}
