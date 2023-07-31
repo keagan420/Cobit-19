72af632783d823cf89c6a3e930f86aad84efc53f
Daniel Coetzee <danicoetzee1@gmail.com>
2023-07-31 02:42:06 +0200

    More small fixes that we dont serve unnecessary content.

--- Cobit-19/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Cobit-19/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -92,7 +92,7 @@ namespace Cobit_19.Areas.Identity.Pages.Account
                 ModelState.AddModelError(string.Empty, ErrorMessage);
             }
 
-            returnUrl ??= Url.Content("~/userhome");
+            returnUrl ??= Url.Content("~/");
 
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
@@ -104,7 +104,7 @@ namespace Cobit_19.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/userhome");
+            returnUrl ??= Url.Content("~/");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
