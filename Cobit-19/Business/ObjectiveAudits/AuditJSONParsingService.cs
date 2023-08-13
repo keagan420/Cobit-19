13f3959fda8e2aa76d9f764d5f356bb7ad57aa5b
Daniel <danicoetzee1@gmail.com>
2023-08-13 13:21:23 +0200

    JSON Parsing -  setup DTOs and JSON parsing serivce.

---
+++ Cobit-19/Business/ObjectiveAudits/AuditJSONParsingService.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using Cobit_19.Shared.Dtos;
+
+namespace Cobit_19.Business.ObjectiveAudits
+{
+    public class AuditJSONParsingService
+    {
+        public static FullObjectiveAuditDto parseAuditTemplate (string jsonTemplate)
+        {
+            FullObjectiveAuditDto objectiveAudit = JsonConvert.DeserializeObject<FullObjectiveAuditDto>(jsonTemplate);
+            return objectiveAudit;
+        }
+    }
+}
---
+++ Cobit-19/Shared/Dtos/ComponentQuestionDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Cobit_19.Shared.Dtos
+{
+    public class ComponentQuestionDto
+    {
+        public string questionType { get; set; }
+        public string questionText { get; set; }
+        public int questionAnswer { get; set; }
+        public float questionScore { get; set; }
+        public string questionComment { get; set; }
+    }
+}
